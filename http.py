e806060e40e0c29ce78b5408223c5580c2c62df6
zalesnyi <zalesnyi@99cadc30-3bf9-11df-9bc1-510916f65ae8>
2015-10-01 13:38:07 +0000

    add checking HTTP response code for correctness and shorcuts for sending GET and POST requests
    git-svn-id: https://svn/IT/az/py/gitlabRestClient@25154 99cadc30-3bf9-11df-9bc1-510916f65ae8

--- http.py
+++ http.py
@@ -4,6 +4,7 @@ working with HTTP requests & responces
 
 import config
 import httplib
+import json
 import urlparse
 
 '''
@@ -15,12 +16,26 @@ def mkConn(url):
 	return conCls(pars.netloc)
 
 '''
-send request to a system and return a response
+send request to a system and return a body of response
 '''
-def sendReq(nam, path, meth = 'GET', body = ''):
+def sendReq(nam, path, isCorrect = lambda c: c < 400, meth = 'GET', body = ''):
 	url = config.getURL(nam)
 	cn = mkConn(url)
 	cn.request(meth, '%s/api/v3/%s' % (url, path), body, {
-		'PRIVATE-TOKEN': config.getToken(nam)
+		'PRIVATE-TOKEN': config.getToken(nam),
+		'Content-Type': 'application/json'
 	})
-	return cn.getresponse()
+	rsp = cn.getresponse()
+	ret = rsp.read()
+	assert isCorrect(rsp.status), '''Server has returned an error
+URL: %s
+Status: %d
+Body: %s
+''' % (url, rsp.status, ret)
+	return json.loads(ret)
+
+# send a GET request
+get = lambda nam, path: sendReq(nam, path, lambda c: c == 200)
+
+# send a POST request
+post = lambda nam, path, hsh: sendReq(nam, path, lambda c: c == 201, 'POST', json.dumps(hsh))
