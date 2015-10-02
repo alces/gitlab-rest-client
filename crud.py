d150cea618a3098c6879f4e13381134142ca17d3
zalesnyi <zalesnyi@99cadc30-3bf9-11df-9bc1-510916f65ae8>
2015-10-02 07:50:06 +0000

    implement an addition of new groups
    git-svn-id: https://svn/IT/az/py/gitlabRestClient@25158 99cadc30-3bf9-11df-9bc1-510916f65ae8

--- crud.py
+++ crud.py
@@ -7,9 +7,15 @@ import http
 class Crud():
 	def __init__(self, path):
 		self.path = path
-		
+
 	'''
 	get an object by system's name and id
 	'''
 	def byId(self, sysNam, id):
 		return http.get(sysNam, '%s/%d' % (self.path, id))
+
+	'''
+	add a new instance of an object
+	'''
+	def add(self, sysNam, data):
+		return http.post(sysNam, self.path, data)
--- groups.py
+++ groups.py
@@ -7,3 +7,9 @@ from crud import Crud
 class groups(Crud):
 	def __init__(self):
 		Crud.__init__(self, 'groups')
+
+	'''
+	add a new group
+	'''
+	def add(self, sysNam, grpNam):
+		return Crud.add(self, sysNam, {'name': grpNam, 'path': grpNam})
