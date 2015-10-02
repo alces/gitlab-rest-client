0725596e99d5b7fddaf51fb0c126a14d45e1b340
zalesnyi <zalesnyi@99cadc30-3bf9-11df-9bc1-510916f65ae8>
2015-10-02 08:51:03 +0000

    implement getting object by name (with caching results)
    git-svn-id: https://svn/IT/az/py/gitlabRestClient@25160 99cadc30-3bf9-11df-9bc1-510916f65ae8

--- crud.py
+++ crud.py
@@ -7,6 +7,7 @@ import http
 class Crud():
 	def __init__(self, path):
 		self.path = path
+		self.cache = {}
 
 	'''
 	get an object by system's name and id
@@ -25,4 +26,23 @@ class Crud():
 	'''
 	def delete(self, sysNam, id):
 		return http.delete(sysNam, '%s/%d' % (self.path, id))
+	
+	'''
+	create a cache of objects
+	'''
+	def mkCache(self, sysNam, keyField = 'name'):
+		if sysNam not in self.cache or not self.cache[sysNam]:
+			self.cache[sysNam] = dict(map(lambda e: (e[keyField], e), http.get(sysNam, self.path)))
+		return self.cache[sysNam]
+	
+	'''
+	clear cache for a given system
+	'''
+	def clrCache(self, sysNam):
+		self.cache[sysNam] = {}
 
+	'''
+	get an object by system's name and an object's name
+	'''
+	def byName(self, sysNam, objNam):
+		return self.mkCache(sysNam)[objNam]
