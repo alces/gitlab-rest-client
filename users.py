0165e300f12d95c9a7d1424c80dd3b493ab02584
zalesnyi <zalesnyi@99cadc30-3bf9-11df-9bc1-510916f65ae8>
2015-10-05 11:06:09 +0000

    move creation of random password to a separate lambda
    git-svn-id: https://svn/IT/az/py/gitlabRestClient@25170 99cadc30-3bf9-11df-9bc1-510916f65ae8

--- users.py
+++ users.py
@@ -5,9 +5,12 @@ working with gitlab's users
 from crud import Crud
 import random
 
-class Users (Crud):
+class Users (Crud):	
 	def __init__(self):
 		Crud.__init__(self, 'users', lambda x: x['username'])
+
+	# generate random password
+	rand_pass = lambda s, l = 10: ''.join(chr(random.randint(64, 122)) for x in xrange(l))
 
 	'''
 	create a new user
@@ -15,5 +18,5 @@ class Users (Crud):
 	def add(self, sysNam, login, fullName, email, **opts):
 		return Crud.add(self, sysNam, dict(
 			[('name', fullName), ('username', login), ('email', email)]
-			+ ('password' in opts and [] or [('password', ''.join(chr(random.randint(64, 122)) for x in xrange(10)))])
+			+ ('password' in opts and [] or [('password', self.rand_pass())])
 			+ opts.items()))
